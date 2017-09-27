103c01353cd719a27f06f6a8f8e147166c1b6821
Aleix Garriga <elvisgarriga@gmail.com>
2017-09-27 16:21:15 +0200

    Creació d'arxiu tipus .java
    S'ha creat un arxiu de programació java, anomenat "suma.java".
    
    Aleix Garriga

---
+++ suma.java
@@ -0,0 +1,8 @@
+int numero1 = 0;
+int numero2 = 0;
+
+System.out.println("Introdueix el primer numero:");
+numero1 = reader.nextInt();
+
+System.out.println("Introdueix el segon numero:");
+numero2 = reader.nextInt();
