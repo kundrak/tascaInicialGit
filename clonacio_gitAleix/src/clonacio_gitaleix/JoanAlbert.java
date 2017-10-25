3efc8b8a6a1f2e0fea5012b6e5a2c06e35d09e32
Joan Albert <Joan Albert@192.168.1.38>
2017-10-25 22:24:29 +0200

    COMMIT JOANALBERT

---
+++ clonacio_gitAleix/src/clonacio_gitaleix/JoanAlbert.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clonacio_gitaleix;
+
+/**
+ *
+ * @author Joan Albert
+ */
+public class JoanAlbert {
+    
+    
+    
+        public JoanAlbert() {
+            System.out.println("HOLA, TEST JOAN ALBERT");
+        }
+}
